90ab959236c7e3c37d8227aeb8ec8bc6b1d023d2
multiangle <461445957@qq.com>
2016-08-30 12:17:34 +0800

    可用版本

--- test.py
+++ test.py
@@ -4,22 +4,8 @@ import re
 import time
 import os
 import asyncio
-
-# ftp = FTP(config.ftp_ip)
-# ftp.login(user=config.ftp_user,passwd=config.ftp_pwd)
-# ftp.cwd(config.ftp_dir)
-#
-#
-# def putin(line):
-#     yield line
-#
-# ret = ftp.nlst()
-# s =[]
-# ftp.retrlines("LIST",s.append)
-# print(s)
-# ftp.putcmd("delete README")
-# print(ftp.getline())
-
+from pprint import pprint
+import logging
 
 
 @asyncio.coroutine
@@ -42,9 +28,14 @@ def local_iterator(path=config.local_dir):
 def local_stat(tmppath):
     os_stat = os.stat(tmppath)
     st_mtime = os_stat.st_mtime
+    rel_path_list = trans_abs_to_rel(config.local_dir,tmppath,os.sep)
+    rel_path = "~"
+    for x in rel_path_list:
+        rel_path += '/' + x
+
     return dict(
         abs_path    = tmppath,
-        rel_path    = trans_abs_to_rel(config.local_dir,tmppath,os.sep),
+        rel_path    = rel_path,
         mtime       = int(st_mtime),
         size        = os_stat.st_size,
         isdir       = os.path.isdir(tmppath)
@@ -59,8 +50,13 @@ def remote_iterator(conn, path=config.ftp_dir):
     for line in lines:
         conn.cwd(path)
         info = parse_ftp_info(line)
-        newpath = path + r'/' + info['name']
+        newpath = path + '/' + info['name']
         info['abs_path'] = newpath
+        rel_path_list = trans_abs_to_rel(config.ftp_dir, newpath, '/')
+        rel_path = '~'
+        for x in rel_path_list:
+            rel_path += '/' + x
+        info['rel_path'] = rel_path
         if info['isdir']:
             yield info
             yield from remote_iterator(conn, path=newpath)
@@ -90,11 +86,167 @@ def trans_abs_to_rel(par_p,abs_p,sep):
     rel_l = abs_l[par_l.__len__():]
     return rel_l
 
-info = ['-rw-------    1 1001     1001         5614 Aug 29 09:38 README', 'drwx------    2 1001     1001         4096 Aug 29 11:41 TEST']
-ftp = FTP(config.ftp_ip)
-ftp.login(user=config.ftp_user,passwd=config.ftp_pwd)
-for x in remote_iterator(ftp):
-    print(x)
+def pull():
+    print('------------------------------------------------')
+    print('start pull at {t}\n'.format(t=gen_time()))
+    ftp = FTP(config.ftp_ip)
+    ftp.encoding = 'utf-8'
+    ftp.login(user=config.ftp_user,passwd=config.ftp_pwd)
+    remote_files = [x for x in remote_iterator(ftp)]
+    local_files = [x for x in local_iterator()]
 
-for x in local_iterator():
-    print(x)
+    # 建立任务列表
+    tasks = pull_task_gen(remote_files, local_files)
+
+    # 根据任务列表进行更新，上传，删除等操作
+    buffsize = 1024
+    for task in tasks:
+        if task['type'] == 'update' or \
+                (task['type'] == 'add' and not task['isdir']): # 普通文件的情况
+            local_file = open(task['to_addr'], 'wb')
+            ftp.retrbinary('RETR '+task['from_addr'], local_file.write, buffsize) # 写入本地文件
+
+        elif task['type'] == 'add' and task['isdir'] : # 要建立文件夹的情况
+            os.mkdir(task['to_addr'])
+
+        elif task['type'] == 'del':
+            addr = task['addr']
+            if os.path.exists(addr):
+                if task['isdir']:
+                    os.rmdir(addr)
+                else:
+                    os.remove(addr)
+
+        else:
+            raise RuntimeError('Unknown task types')
+
+    ftp.close()
+
+def pull_task_gen(remote_files, local_files):
+    tasks = []
+    local_left_files = local_files[:]
+    remote_rel_list = [x['rel_path'] for x in remote_files]
+    local_rel_list = [x['rel_path'] for x in local_files]
+    for remote_file in remote_files:
+        remote_rel_path = remote_file['rel_path']
+        if remote_rel_path in local_rel_list: # 如果本地有该文件
+            local_left_rel_paths = [x['rel_path'] for x in local_left_files]
+            local_left_files.pop(local_left_rel_paths.index(remote_rel_path))
+            local_file = local_files[local_rel_list.index(remote_rel_path)]
+            if local_file['isdir']:
+                continue
+            if remote_file['mtime'] - local_file['mtime'] > 60 :  # 如果ftp端文件比本地要提前N秒，则进行同步
+                task = dict(
+                    type        = 'update',
+                    from_addr   = remote_file['abs_path'],
+                    to_addr     = local_file['abs_path']
+                )
+                tasks.append(task)
+            else:
+                pass
+        else:   # 如果本地没有文件
+            rel_path = remote_file['rel_path']
+            rel_path_list = rel_path.split('/')
+            rel_path_list = rel_path_list[1:]
+            local_abs_path = config.local_dir
+            for x in rel_path_list:
+                local_abs_path += os.sep + x
+            task = dict(
+                type        = 'add',
+                from_addr   = remote_file['abs_path'],
+                to_addr     = local_abs_path,
+                isdir       = remote_file['isdir']
+            )
+            tasks.append(task)
+
+    for left_file in local_left_files[::-1]:
+        task = dict(
+            type    = 'del',
+            addr    = left_file['abs_path'],
+            isdir   = left_file['isdir']
+        )
+        tasks.append(task)
+    return tasks
+
+def push():
+    print('------------------------------------------------')
+    print('start push at {t}\n'.format(t=gen_time()))
+    ftp = FTP(config.ftp_ip)
+    ftp.encoding = 'utf-8'
+    ftp.login(user=config.ftp_user,passwd=config.ftp_pwd)
+    remote_files = [x for x in remote_iterator(ftp)]
+    local_files = [x for x in local_iterator()]
+
+    tasks = push_task_gen(local_files, remote_files)
+
+    buffsize = 1024
+    for task in tasks:
+        if task['type'] == 'update' or \
+                (task['type'] == 'add' and not task['isdir']): # 普通文件的情况
+            local_file = open(task['from_addr'], 'rb')
+            print(task['from_addr'])
+            ftp.storbinary('STOR ' + task['to_addr'], local_file, buffsize) #  上传文件
+
+        elif task['type'] == 'add' and task['isdir'] : # 要建立文件夹的情况
+            ftp.mkd(task['to_addr'])
+
+        elif task['type'] == 'del':
+            addr = task['addr']
+            if task['isdir']:
+                ftp.rmd(addr)
+            else:
+                print(addr)
+                ftp.delete(addr)
+
+    ftp.close()
+
+
+def push_task_gen(local_files, remote_files):
+    tasks = []
+    remote_left_files = remote_files[:]
+    remote_rel_list = [x['rel_path'] for x in remote_files]
+    for local_file in local_files:
+        local_rel_path = local_file['rel_path']
+        if local_rel_path in remote_rel_list:  # 如果ftp端有该文件
+            remote_left_rel_paths = [x['rel_path'] for x in remote_left_files]
+            remote_left_files.pop(remote_left_rel_paths.index(local_rel_path))
+            remote_file = remote_files[remote_rel_list.index(local_rel_path)]
+            if remote_file['isdir']:
+                continue
+            if local_file['mtime'] - remote_file['mtime'] > 60:
+                task = dict(
+                    type        = 'update',
+                    from_addr   = local_file['abs_path'],
+                    to_addr     = remote_file['abs_path']
+                )
+                tasks.append(task)
+            else:
+                pass
+        else:   # 如果ftp端没有文件
+            rel_path = local_file['rel_path']
+            rel_path_list = rel_path.split('/')
+            rel_path_list = rel_path_list[1:]
+            remote_abs_path = config.ftp_dir
+            for x in rel_path_list:
+                remote_abs_path += '/' + x
+            task = dict(
+                type        = 'add',
+                from_addr   = local_file['abs_path'],
+                to_addr     = remote_abs_path,
+                isdir       = local_file['isdir']
+            )
+            tasks.append(task)
+    for left_file in remote_left_files[::-1]:
+        task = dict(
+            type    = 'del',
+            addr    = left_file['abs_path'],
+            isdir   = left_file['isdir']
+        )
+        tasks.append(task)
+    return tasks
+
+def gen_time():
+    t = time.localtime(time.time())
+    return time.strftime('%Y-%m-%d %H:%M:%S', t)
+
+push()
